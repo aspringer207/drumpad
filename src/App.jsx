0a6ddf2aaa4ccf62edffd232b016d98e12e5f586
aspringer207 <andrea.m.springer@gmail.com>
2024-02-24 16:42:19 -0500

    add sound src refs

--- src/App.jsx
+++ src/App.jsx
@@ -12,15 +12,16 @@ import { Form, FormGroup, FormLabel, Card, AlertHeading } from 'react-bootstrap'
 function App() {
   return (
     <>
-      <div class="settingsBar">
+    <Col id="drum-machine">
+      <Row class="settingsBar">
         <div id="volume-wrapper">
           <FormGroup>
             <FormLabel>Volume</FormLabel>
             <Form.Range id="volume" defaultValue="50"></Form.Range>
           </FormGroup>
         </div>
-      </div>
-      <Card>
+      </Row>
+      <Row>
           <Alert id="display" variant="info">
             <AlertHeading>Hey, nice to see you</AlertHeading>
             <p>
@@ -34,33 +35,33 @@ function App() {
               nice and tidy.
             </p>
           </Alert>
-      </Card>
-      <Col id="drum-machine">
+      </Row>
+      
         
         <Row>
           <h1>DrumPad</h1>
         </Row>
         <Row>
           <ButtonGroup size="lg">
-            <Button className="drum-pad" variant="warning" id="sticks">Q<audio href="src/assets/sticks.wav" id="q"></audio></Button>
-            <Button className="drum-pad" variant="danger" id="hihat">W<audio href="src/assets/ZildMstrsnd-HiHat0.wav" id="w"></audio></Button>
-            <Button className="drum-pad" variant="primary" id="crash">E<audio href="src/assets/ZildjinCrsh 1-Dyn09.wav" id="e"></audio></Button>
+            <Button className="drum-pad" variant="warning" id="sticks">Q<audio type="audio/wav" src="sticks.wav" href="src/assets/sticks.wav" id="q"></audio></Button>
+            <Button className="drum-pad" variant="danger" id="hihat">W<audio type="audio/wav" src="ZildMstrsnd-HiHat0.wav" href="src/assets/ZildMstrsnd-HiHat0.wav" id="w"></audio></Button>
+            <Button className="drum-pad" variant="primary" id="crash">E<audio type="audio/wav" src="ZildjinCrsh 1-Dyn09.wav" href="src/assets/ZildjinCrsh 1-Dyn09.wav" id="e"></audio></Button>
 
           </ButtonGroup>
 
         </Row>
         <Row>
           <ButtonGroup>
-            <Button className="drum-pad" variant="primary" size="lg" id="ride">A <audio id="a" href="src/assets/SabHHXEvo20_Bell05.wav"></audio></Button>
-            <Button className="drum-pad" variant="info" id="snare" size="lg">S <audio id="s" href="src/assets/drum_snare_hard.wav"></audio></Button>
-            <Button className="drum-pad" variant="danger" id="bass" size="lg">D <audio id="d" href="src/assets/drum_bass_hard.wav"></audio></Button>
+            <Button className="drum-pad" variant="primary" size="lg" id="ride">A <audio type="audio/wav"id="a" src="SabHHXEvo20_Bell05.wav" href="src/assets/SabHHXEvo20_Bell05.wav"></audio></Button>
+            <Button className="drum-pad" variant="info" id="snare" size="lg">S <audio type="audio/wav" id="s" src="drum_snare_hard.wav" href="src/assets/drum_snare_hard.wav"></audio></Button>
+            <Button className="drum-pad" variant="danger" id="bass" size="lg">D <audio type="audio/wav" id="d" src="drum_bass_hard.wav" href="src/assets/drum_bass_hard.wav"></audio></Button>
           </ButtonGroup>
         </Row>
         <Row>
           <ButtonGroup size="lg">
-            <Button className="drum-pad" variant="info" id="lowtom">Z <audio id="z" href="src/assets/drum_tom_lo_hard.wav" ></audio></Button>
-            <Button className="drum-pad" variant="warning" id="midtom">X <audio id="x" href="src/assets/drum_tom_mid_hard.wav"></audio></Button>
-            <Button className="drum-pad" variant="primary" id="hitom">C <audio id="c" href="src/assets/drum_tom_hi_hard.wav"></audio></Button>
+            <Button className="drum-pad" variant="info" id="lowtom">Z <audio type="audio/wav" src="drum_tom_lo_hard.wav" id="z" href="src/assets/drum_tom_lo_hard.wav" ></audio></Button>
+            <Button className="drum-pad" variant="warning" id="midtom">X <audio type="audio/wav" src="drum_tom_mid_hard.wav" id="x" href="src/assets/drum_tom_mid_hard.wav"></audio></Button>
+            <Button className="drum-pad" variant="primary" id="hitom">C <audio type="audio/wav" src="drum_tom_hi_hard.wav" id="c" href="src/assets/drum_tom_hi_hard.wav"></audio></Button>
           </ButtonGroup>
         </Row>
 
