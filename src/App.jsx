6d59853814a9faa72140a5aef6d05cf9a44b2c3d
aspringer207 <andrea.m.springer@gmail.com>
2024-02-24 16:25:10 -0500

    fix imports

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Col from 'react-bootstrap/Col';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
-import { Form, FormGroup, FormLabel } from 'react-bootstrap';
+import { Form, FormGroup, FormLabel, Card, Alert, AlertHeading } from 'react-bootstrap';
 
 function App() {
   const [volume, setVolume] = useState(0)
@@ -19,12 +19,28 @@ function App() {
         <div id="volume-wrapper">
           <FormGroup>
             <FormLabel>Volume</FormLabel>
-            <Form.Range id="volume" defaultValue="50" currentValue={null}></Form.Range>
+            <Form.Range id="volume" defaultValue="50"></Form.Range>
           </FormGroup>
 
         </div>
       </div>
+      <Card>
+          <Alert id="display" variant="info">
+            <AlertHeading>Hey, nice to see you</AlertHeading>
+            <p>
+              Aww yeah, you successfully read this important alert message. This
+              example text is going to run a bit longer so that you can see how
+              spacing within an alert works with this kind of content.
+            </p>
+            <hr />
+            <p className="mb-0">
+              Whenever you need to, be sure to use margin utilities to keep things
+              nice and tidy.
+            </p>
+          </Alert>
+        </Card>
       <Col id="drum-machine">
+        
         <Row>
           <h1>DrumPad</h1>
         </Row>
@@ -39,18 +55,17 @@ function App() {
         </Row>
         <Row>
           <ButtonGroup>
-            <Button className="drum-pad" variant="primary" size="lg" id="ride">A <audio id="a"href="src/assets/SabHHXEvo20_Bell05.wav"></audio></Button>
+            <Button className="drum-pad" variant="primary" size="lg" id="ride">A <audio id="a" href="src/assets/SabHHXEvo20_Bell05.wav"></audio></Button>
             <Button className="drum-pad" variant="info" id="snare" size="lg">S <audio id="s" href="src/assets/drum_snare_hard.wav"></audio></Button>
             <Button className="drum-pad" variant="danger" id="bass" size="lg">D <audio id="d" href="src/assets/drum_bass_hard.wav"></audio></Button>
           </ButtonGroup>
         </Row>
         <Row>
           <ButtonGroup size="lg">
-            <Button className="drum-pad" variant="info" size="lg" id="lowtom">Z <audio id="z" href="src/assets/drum_tom_lo_hard.wav" ></audio></Button>
-            <Button className="drum-pad" variant="warning" size="lg" id="midtom">X <audio id="x" href="src/assets/drum_tom_mid_hard.wav"></audio></Button>
-            <Button className="drum-pad" variant="primary" size="lg" id="hitom">C <audio id="c" href="src/assets/drum_tom_hi_hard.wav"></audio></Button>
+            <Button className="drum-pad" variant="info" id="lowtom">Z <audio id="z" href="src/assets/drum_tom_lo_hard.wav" ></audio></Button>
+            <Button className="drum-pad" variant="warning" id="midtom">X <audio id="x" href="src/assets/drum_tom_mid_hard.wav"></audio></Button>
+            <Button className="drum-pad" variant="primary" id="hitom">C <audio id="c" href="src/assets/drum_tom_hi_hard.wav"></audio></Button>
           </ButtonGroup>
-
         </Row>
 
 
